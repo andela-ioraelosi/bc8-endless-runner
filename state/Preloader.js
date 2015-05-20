23b41f724ce61eb64b00923e2ef7394cc2522537
Chijioke Ndubisi <cjndubisi@gmail.com>
2015-05-20 19:42:27 +0100

    load images for splash

--- state/Preloader.js
+++ state/Preloader.js
@@ -1,23 +1,28 @@
 Runner.Preloader = function (game) {
-    this.preloadBar = null;
-    this.title.text = null;
     this.ready  = false 
-    
 }
 
 Runner.Preloader.prototype = {
     
     preload: function (){
-        // add images loaded in boot preload as sprite
+        // add images loaded in boot preload 
+        this.load.image('characterimg','assets/splash/gb_0009_Layer-5.png');
+        
+        this.load.spritesheet('map', 'assets/splash/gb_0007_map.png');
+        this.load.image('logo', 'assets/splash/gb_0008_logo.png');
+        this.load.image('spikes', 'assets/splash/gb_0010_spikes.png');
+        this.load.image('tree', 'assets/splash/gb_0011_trees-copy.png')
         // load add sprites 
+        this.load.spritesheet('startbtn', 'assets/splash/gb_0003_button-start.png',138,38);
+
     },
     
     create: function(){
-        // disable crop of image since it will be 100%
-        this.preloadBar.setCropEnabled = false;
+
     },
 
     update: function(){
         this.ready = true;
+        this.state.start('StartMenu');
     }
 }
