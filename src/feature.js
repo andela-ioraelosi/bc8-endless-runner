8f241e884e5a9602a9f01cb5f4648bba4503695f
Akinwoleola Opeyemi <akinwoleolaopeyemi@gmail.com>
2015-05-20 18:56:05 +0100

    Add audio files in asset directory

---
+++ src/feature.js
@@ -0,0 +1,13 @@
+var myApp = angular.module("myApp", ["firebase"]);
+
+if(game.end === true){
+	$scope.
+}
+
+$scope.authObj.$onAuth(function(authData) {
+  if (authData) {
+    console.log(authData.uid +" "+  authData.uid.child(score));
+  } else {
+    console.log("Logged out");
+  }
+});
