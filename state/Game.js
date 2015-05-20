66886a803fe0c43161d88aa0fa8a21f2ac72c67a
Chijioke Ndubisi <cjndubisi@gmail.com>
2015-05-20 19:42:01 +0100

    add game map sprite sheet

--- state/Game.js
+++ state/Game.js
@@ -1,7 +1,17 @@
-Runner.game = function (game){}
+Runner.Game = function (game){
+    this.mapGroup = null;
+    this.map = null;
+}
 
-Runner.game.prototype = {
-    create: function (){},
-    update: function () {}
+Runner.Game.prototype = {
+    create: function (){
+        this.mapGroup = this.add.group()
+        this.map = this.add.tileSprite(this.world.width, this.world.height,736, 111, 'map');
+        this.mapGroup.add(this.map)
+
+    },
+    update: function () {
+        
+    }
     
 };
