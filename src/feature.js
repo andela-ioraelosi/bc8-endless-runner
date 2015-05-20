49aaf942fc0f3478efba18bb6f874d0fce94c88d
Akinwoleola Opeyemi <akinwoleolaopeyemi@gmail.com>
2015-05-20 19:34:18 +0100

    Log user score on authentication

--- src/feature.js
+++ src/feature.js
@@ -1,12 +1,21 @@
 var myApp = angular.module("myApp", ["firebase"]);
 
-if(game.end === true){
-	$scope.
+/*if(game.end === true){
+	$scope.push();
 }
 
+var list = $firebaseArray(ref);
+list.$add({ foo: "bar" }).then(function(ref) {
+  var id = ref.key();
+  console.log("added score with id " + id);
+  list.$indexFor(id); // returns location in the array
+});*/
+
+
+var scoreX = rootRef.child("Users").child(authData.uid).child("score")
 $scope.authObj.$onAuth(function(authData) {
   if (authData) {
-    console.log(authData.uid +" "+  authData.uid.child(score));
+    console.log(authData.uid +" "+  scoreX);
   } else {
     console.log("Logged out");
   }
