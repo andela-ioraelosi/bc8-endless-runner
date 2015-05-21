73c853dfde8924ed20549a5bb4f76ca20f74c119
Chijioke Ndubisi <cjndubisi@gmail.com>
2015-05-21 15:10:31 +0100

    directory restructure

---
+++ js/login.js
@@ -0,0 +1,53 @@
+<script type="application/javascript">          
+           var rootRef = new Firebase("https://quicktest-app.firebaseio.com"),
+           leaderBoardRef = rootRef.child('leaderBoard');
+           var email,password,username, userObj = {};
+
+           $(document).ready(function(){
+                $('#overlay').show();
+                $('#login_box').show();
+            });
+            //SIGN UP SECTION
+            $('#signUp').click(function(){
+                //alert('you clicked me');
+                email = $('#userEmail').val();
+                password = $('#userPassword').val();
+                userObj = {email: email, password: password};
+                rootRef.createUser(userObj, function(error, userData) {
+                  if (error) {
+                    console.log("Error creating user:", error);
+                    $('#status').append(error);//logs any errors for user
+                  } else {
+                    console.log("Successfully created user account with uid:", userData.uid);
+                    //create user profile following successful signup/authentication
+                    var username = email.slice(0, email.indexOf('@')); 
+                   // rootRef.onAuth(function(authData){
+                        rootRef.child("users").child(username).set({
+                             name: username,
+                            score: [], highScore: 0});
+                   // });//end of onAuth
+                $('#status').append('<h3>Youre signed Up!!! Click Login to proceed.</h3>');
+                //logs to user that sign up is successful
+                  }//end of else
+                });//end of refcreateUser
+            });//end of signup
+            
+          //LOGIN SECTION  
+            $('#logIn').click(function(){
+                //alert('you clicked me');
+                email = $('#userEmail').val();
+                password = $('#userPassword').val();
+                userObj = {email: email, password: password};
+                rootRef.authWithPassword(userObj, function(error, authData) {
+                    if(error) {
+                            console.log("Login failed!", error);
+                        } else {
+                            console.log("Authenticated successfully with payload",authData);
+                            //if auth succesful switch to this page
+                            //window.location.href="https://google.com"
+                            $('#overlay').hide();
+                            $('#login_box').hide();
+                        }  
+                });//end of authWithPassword
+            });//end of Login
+    </script>
