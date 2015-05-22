d0067d0fdc5d0fb359c5a5225bb03a6172516c8f
Akinwoleola Opeyemi <akinwoleolaopeyemi@gmail.com>
2015-05-22 10:33:06 +0100

    update for score position and save score button

--- js/getScore.js
+++ js/getScore.js
@@ -1,25 +1,27 @@
 var rootRef = new Firebase('https://quicktest-app.firebaseio.com/users');
-$('#logIn').click( function(){
-  var scoreSnapshot;
+ var authData = rootRef.getAuth();
+ var scoreSnapshot;
+ if (authData){
   rootRef.once('value', function(snapshot) {
   // store dataSnapshot for use in below examples.
   scoreSnapshot = snapshot.val();
   console.log(scoreSnapshot);
+  } 
+ 
 
-  var $scoreTrack = $('#userscore')
- $scoreTrack.text(scoreSnapshot);
-  })
-});
+  //var $scoreTrack = $('#userscore')
+// $scoreTrack.text(scoreSnapshot);
+//});
 
-$('#userScore').click( function(){
- var scoreRef = rootRef.child('users').child('score');
+$('#userScore').click(function(){
+ var scoreRef = rootRef.child('users').child(test).child('score');
   scoreRef.update({
   //name for defined variable that stores score at end of game
-    "score": 
+    "score": 70
       });
-  } else {
-    window.alert("Not Updated");
-    }
+  //else {
+    //window.alert("Not Updated");
+    //}
   
   });
 // }
